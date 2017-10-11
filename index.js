2d6a037002ad7e847363a3f71a1df246823a3799
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2017-10-11 15:42:34 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const CHARS = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+
+// throws an error if len cannot be coerced to a number
+// NOTE: null and [] are coerced to 0 by unary +
+function GetRandomString (len) {
+  len = +len;
+  if (isNaN(len)) throw new TypeError(`Argument 'len' must be a number!`);
+
+  const n = CHARS.length;
+  
+  let ret = "";
+  for (let i = 0; i < len; i++) ret += CHARS[Math.floor(Math.random() * n)];
+  return ret;
+}
+
+module.exports = GetRandomString;
+
